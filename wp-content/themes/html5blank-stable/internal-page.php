0332d12e718664f971cda5d58fc0df0ab13b2b34
mreese <mreese@element84.com>
2015-02-03 12:04:05 -0600

    Working navigation

--- wp-content/themes/html5blank-stable/internal-page.php
+++ wp-content/themes/html5blank-stable/internal-page.php
@@ -1,10 +1,10 @@
-<?php get_header(); ?>
+<?php /* Template Name: Internal Page Template */ get_header(); ?>
 
   <main role="main">
     <!-- section -->
     <section>
 
-      <!--<h1><?php the_title(); ?></h1>-->
+      <h1><?php the_title(); ?></h1>
 
     <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
@@ -12,6 +12,8 @@
       <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
         <?php the_content(); ?>
+
+        <?php comments_template( '', true ); // Remove if you don't want comments ?>
 
         <br class="clear">
 
